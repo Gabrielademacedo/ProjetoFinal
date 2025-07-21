50c8c01aadb041dfd4b5cbfe3da21260ff992852
Gabrielademacedo <gabimr1034@gmail.com>
2025-07-21 10:13:08 -0300

    ajustando os cards dos carros na pagina inicial

--- src/app/components/cards-carros/cards-carros.ts
+++ src/app/components/cards-carros/cards-carros.ts
@@ -1,13 +1,14 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, input } from "@angular/core";
 
 @Component({
-  selector: 'app-cards-carros',
+  selector: "app-cards-carros",
   imports: [],
-  templateUrl: './cards-carros.html',
-  styleUrl: './cards-carros.css',
+  templateUrl: "./cards-carros.html",
+  styleUrl: "./cards-carros.css",
 })
 export class CardsCarros {
   @Input() imagem?: string;
   @Input() titulo?: string;
   @Input() descricao?: string;
+  @Input() link?: string;
 }
